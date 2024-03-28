f0589bdbcd7ebe7924abfa6d6aaa8a04ab1863f2
Dzmitry Belanovich <belanovich_dzmitry@mail.ru>
2024-03-28 15:58:41 +0200

    task 1/1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,12 @@
 function factorial(n) {
-  // ваш код...
+  let i = 1;
+  while (n != 0) {
+    i = i * n--;
+  }
+  return i;
+  /*let i;
+  for (i = 1; n != 0; ) {
+    i = i * n--;
+  }
+  return i;*/
 }
